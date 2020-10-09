d0ecbb5e6bf6fd17889f76554774bd13f4d7c441
senseiHector <amoahhector@live.com>
2020-10-09 14:30:23 +0000

    Removed Lecturer

---
+++ src/BagOfLectures.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+public class BagOfLectures {
+    private HashSet<Lecture> bag;
+
+    public BagOfLectures(){
+        bag = new HashSet<>();
+    }
+
+    public boolean add(Lecture lecture){
+        return false;
+    }
+    public boolean remove(Lecture lecture){
+        return false;
+    }
+
+    public void clear(){
+
+    }
+}
